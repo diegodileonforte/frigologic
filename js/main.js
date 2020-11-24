a1f3abce6619c4438995d4d831f1cabc6135cc9c
Diego Di Leonforte <dileonfortediego@gmail.com>
2020-11-24 20:02:52 -0300

    ajustes de estilos y botones de imprimir y guardar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let miAmbiente = new Ambiente();
 
-$(".temp_btn_onclick").click((evento)=>{
+$(".temp_btn_onclick").click((evento) => {
     miAmbiente.setTemperatura(evento.target.parentNode.value)
 });
 
@@ -12,8 +12,33 @@ $("#ventanas").change(miAmbiente.setVentanas);
 $("#ocupantes").change(miAmbiente.setOcupantes);
 $("#calcular").click(miAmbiente.calculoGlobal);
 
-/* // //Botón imprimir//
-function imprimir() {
-    window.print();
+//Botón imprimir//
 
-//Botón guardar// */
+$("#imprimir").click(() => {
+    window.print();
+})
+
+
+//Botón guardar//
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+document.getElementById("guardar").addEventListener("click", () => {
+
+    var text = "Estimado usuario: \nPara su habitación de " + $("#SuperficieTotal").val() + " m3 le recomendamos un equipo de " + $("#frigorias").val() + " frigorias " + "\(" + $("#Watts").val() + " Watts/" + $("#Kcal").val() + " Kcal/" + $("#BTU").val() + " BTU" + "\).\n" + "Ante cualquier duda, escríbanos a ayuda@grigologic.com.";
+    var filename = "frigologic.txt";
+
+    download(filename, text);
+
+}, false);
